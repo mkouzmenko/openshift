076ebb4eebd524dbd3de19843b98cb0d401c6856
mkouzmenko <mkouzmenko@gmail.com>
2024-09-20 11:32:23 -0400

    Update app.py

--- s2i/python/app.py
+++ s2i/python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello world from Python\n"
+    return "Hello world from Python 1\n"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=False)
